76944a86e871700b8d8db362c906369de1f9ae59
Ivan-Lomando <ilomandofernandez@gmail.com>
2024-09-18 19:09:18 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+const Ta1 = ({titulo,descripcion,persona,inicio,fin}) => {
+  return (
+    <div className='card'>
+      <h1>{titulo}</h1>
+      <p>Descripcion: {descripcion}</p>
+      <p>Persona: {persona}</p>
+      <p>Inicio: {inicio}</p>
+      <p>Fin: {fin}</p>
+    </div>
+
+  );
+}
+const Ta2 = ({ children }) => {
+  return (
+    <div className='card'>
+      {children}
+    </div>
+  );
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Ta1 titulo="hola" descripcion="probando" persona="ivan" inicio="hoy" fin="mañana"/>
+       <Ta1 titulo="hola2" descripcion="probandooooo..." persona="ivan" inicio="5/08/2004" fin="mañana"/>
+       <Ta1 titulo="hola3" descripcion="probando siuu" persona="nadie" inicio={5} fin="mañana"/>
+       <Ta2>
+        <h1>hola</h1>
+        <p>Descripcion: probando</p>
+        <p>Persona: ivan</p>
+        <p>Inicio: hoy</p>
+        <p>Fin: mañana</p>
+      </Ta2>
+
     </div>
   );
 }
